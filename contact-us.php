ec6532baf1e005a3fe86cd6d48dd2f4bd39355b7
Rakshit052005 <rakshitgarg052005@gmail.com>
2025-06-17 15:54:49 +0530

    Normalize line endings to LF

---
+++ contact-us.php
@@ -0,0 +1,41 @@
+<?php include('include/header.php'); ?>
+
+       <div class="jumbotron">
+           <h1 class="text-center mt-5">Contact us</h1>
+       </div>
+       
+        <div class="container mt-3">
+          <div class="row">
+            <div class="col-md-6">
+              <h3>Our Office</h3>
+              <hr>
+              <p>Kharar ,Punjab</p>
+            </div>
+            <div class="col-md-6">
+                  <form action="" method="" class=" p-3">
+                    <div class="form-group">
+                      <input type="text" name="Fullname" placeholder="Full Name" class="form-control">
+                     </div>
+                     
+                     <div class="form-group">
+                      <input type="text" name="email" placeholder="Email" class="form-control">
+                     </div>
+                     
+                     <div class="form-group">
+                      <textarea class="form-control" rows="5" cols="20" placeholder="Message"></textarea>
+                    </div>
+
+                      <div class="form-group text-center mt-4">
+                        <input type="submit" name="sigin" class="btn btn-primary" value="Send">
+                      </div>
+
+                  </form>
+                </div>
+         </div>
+          
+           <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13717.058101303877!2d76.6464769!3d30.7390703!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390fef12fb12ffa3%3A0x89255bc077ac8105!2sPicasso%20Floors!5e0!3m2!1sen!2sin!4v1701077656056!5m2!1sen!2sin" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe> 
+           
+         
+       </div>
+         
+       <?php include('include/footer.php');?>
